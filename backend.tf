8b9484b9b1df7fe6b8bbdc357d42016d31da9acf
Hafsa4 <hafsabegum0357@gmail.com>
2026-07-13 23:05:31 +0530

    made chnages

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "hafsa-terraform-state"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
+    dynamodb_table = "terraform_lock_78"
     encrypt        = true
 
   }
