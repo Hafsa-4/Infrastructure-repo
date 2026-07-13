dc894a0502e91e7d55c3bf488031392ed1ffa3a9
Hafsa4 <hafsabegum0357@gmail.com>
2026-07-13 20:36:53 +0530

    made changes

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
-    backend "s3" {
-    bucket = "hafsa-terraform-state"
-    key    = "terraform.tfstate"
-    region = "ap-south-1"
+  backend "s3" {
+    bucket         = "hafsa-terraform-state"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
-    encrypt = true
+    encrypt        = true
 
- }
+  }
 }
