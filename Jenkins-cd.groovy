6b28ba8eedd1aa8e363698146420fd089c58275e
Hafsa4 <hafsabegum0357@gmail.com>
2026-07-14 00:13:30 +0530

    fixed errors

--- Jenkins-cd.groovy
+++ Jenkins-cd.groovy
@@ -1,4 +1,15 @@
-stage('Terraform Init & Plan') {
+pipeline {
+    agent any
+    environment {
+        AWS_DEFAULT_REGION = 'ap-south-1'
+    }
+    stages {
+        stage('Checkout') {
+            steps { 
+                checkout scm 
+            }
+        }
+        stage('Terraform Init & Plan') {
             steps {
                 withVault(configuration: [vaultUrl: 'http://127.0.0.1:8200', vaultCredentialId: 'vault-token'],
                            vaultSecrets: [[path: 'secret/aws-credentials', secretValues: [
@@ -6,7 +17,6 @@ stage('Terraform Init & Plan') {
                                [envVar: 'AWS_SECRET_ACCESS_KEY', vaultKey: 'secret_key']]]]) {
                     
                     echo 'Initializing Backend...'
-                    // Added -migrate-state to auto-approve moving the state to S3
                     sh 'terraform init -input=false -migrate-state'
                     
                     echo 'Generating Execution Plan...'
@@ -14,3 +24,23 @@ stage('Terraform Init & Plan') {
                 }
             }
         }
+        stage('Approval Gate') {
+            steps {
+                // This makes the CD process manual: it completely pauses here for your confirmation
+                input message: 'Do you want to deploy these infrastructure changes to AWS?', ok: 'Yes, Deploy!'
+            }
+        }
+        stage('Terraform Apply') {
+            steps {
+                withVault(configuration: [vaultUrl: 'http://127.0.0.1:8200', vaultCredentialId: 'vault-token'],
+                           vaultSecrets: [[path: 'secret/aws-credentials', secretValues: [
+                               [envVar: 'AWS_ACCESS_KEY_ID', vaultKey: 'access_key'],
+                               [envVar: 'AWS_SECRET_ACCESS_KEY', vaultKey: 'secret_key']]]]) {
+                    
+                    echo 'Applying Infrastructure Changes...'
+                    sh 'terraform apply -auto-approve tfplan'
+                }
+            }
+        }
+    }
+}
