ff047748529aad76400ce2e34fd912dcad7bb85e
Hafsa4 <hafsabegum0357@gmail.com>
2026-07-13 20:15:30 +0530

    edited file

--- variables.tf
+++ variables.tf
@@ -2,11 +2,6 @@ variable "ec2_instance_type" {
   default = "t3.micro"
   type    = string
 }
-variable "ec2_root_storage_size" {
-  default = 8
-  type    = number
-}
-
 variable "ec2_ami_id" {
   default = "ami-0a9723306502e2558"
   type    = string
