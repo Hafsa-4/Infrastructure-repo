37c341cff082ae9868f10d072c74ba54a7aef6a1
Hafsa4 <hafsabegum0357@gmail.com>
2026-07-16 01:05:01 +0530

    fixing  error

--- outputs.tf
+++ outputs.tf
@@ -13,7 +13,7 @@ output "ec2_private_dns" {
   value = aws_instance.terra-project.private_dns
 }
 output "aws_vpc_id" {
- value = aws_vpc.terra-project.id
+  value = aws_vpc.terra-project.id
 }
 output "aws_subnet_id" {
   value = aws_subnet.terra-project.id
@@ -21,7 +21,7 @@ output "aws_subnet_id" {
 output "key_pair_name" {
   value = aws_key_pair.terra-project.key_name
 }
-output "internet_gateway_id"{
+output "internet_gateway_id" {
   value = aws_internet_gateway.terra-project.id
 }
 output "route_table_id" {
