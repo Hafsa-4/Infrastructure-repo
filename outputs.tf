ccaf638ffd7bc308304a82ee604a66b5fe48f55d
Hafsa4 <hafsabegum0357@gmail.com>
2026-07-15 20:13:03 +0530

    added code

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,21 @@ output "ec2_public_dns" {
 output "ec2_private_ip" {
   value = aws_instance.terra-project.private_ip
 }
+output "ec2_private_dns" {
+  value = aws_instance.terra-project.private_dns
+}
+output "aws_vpc_id" {
+ value = aws_vpc.terra-project.id
+}
+output "aws_subnet_id" {
+  value = aws_subnet.terra-project.id
+}
+output "key_pair_name" {
+  value = aws_key_pair.terra-project.key_name
+}
+output "internet_gateway_id"{
+  value = aws_internet_gateway.terra-project.id
+}
+output "route_table_id" {
+  value = aws_route_table.terra-project.id
+}
