59e9e923d6e38290edff128a777caebf36728ff9
Hafsa4 <hafsabegum0357@gmail.com>
2026-07-17 18:49:13 +0530

    changes

--- Jenkins-cd.groovy
+++ Jenkins-cd.groovy
@@ -5,29 +5,13 @@ pipeline {
     }
     stages {
         stage('Checkout') {
-            steps { 
-                checkout scm 
-            }
-        }
-        stage('Terraform Init & Plan') {
             steps {
-                withVault(configuration: [vaultUrl: 'http://65.2.34.167:8200', vaultCredentialId: 'vault-token'],
-                           vaultSecrets: [[path: 'secret/aws-credentials', secretValues: [
-                               [envVar: 'AWS_ACCESS_KEY_ID', vaultKey: 'access_key'],
-                               [envVar: 'AWS_SECRET_ACCESS_KEY', vaultKey: 'secret_key']]]]) {
-                    
-                    echo 'Initializing Backend...'
-                    sh 'terraform init -input=false -migrate-state -force-copy'
-                    
-                    echo 'Generating Execution Plan...'
-                    sh 'terraform plan -out=tfplan'
-                }
+                checkout scm
             }
         }
         stage('Approval Gate') {
             steps {
-                // This makes the CD process manual: it pauses here for your approval
-                input message: 'Do you want to deploy these infrastructure changes to AWS?', ok: 'Yes, Deploy!'
+                input message: 'Deploy these infrastructure changes to AWS?', ok: 'Yes, Deploy!'
             }
         }
         stage('Terraform Apply') {
@@ -36,9 +20,9 @@ pipeline {
                            vaultSecrets: [[path: 'secret/aws-credentials', secretValues: [
                                [envVar: 'AWS_ACCESS_KEY_ID', vaultKey: 'access_key'],
                                [envVar: 'AWS_SECRET_ACCESS_KEY', vaultKey: 'secret_key']]]]) {
-                    
-                    echo 'Applying Infrastructure Changes...'
-                    sh 'terraform apply -auto-approve tfplan'
+
+                    sh 'terraform init'
+                    sh 'terraform apply -auto-approve'
                 }
             }
         }
--- Jenkins-ci.groovy
+++ Jenkins-ci.groovy
@@ -2,7 +2,6 @@ pipeline {
     agent any
     environment {
         AWS_DEFAULT_REGION = 'ap-south-1'
-        VAULT_ADDR = 'http://54.221.153.100:8200'
     }
     stages {
         stage('Checkout') {
@@ -10,38 +9,17 @@ pipeline {
                 checkout scm
             }
         }
-
-        stage('Format Check') {
-            steps {
-                sh 'terraform fmt -check -recursive'
-            }
-        }
-
         stage('Terraform Work') {
             steps {
-                withVault(
-                    configuration: [
-                        vaultUrl: 'http://65.2.34.167:8200',
-                        vaultCredentialId: 'vault-token'
-                    ],
-                    vaultSecrets: [
-                        [path: 'secret/aws-credentials', secretValues: [
-                            [envVar: 'AWS_ACCESS_KEY_ID', vaultKey: 'access_key'],
-                            [envVar: 'AWS_SECRET_ACCESS_KEY', vaultKey: 'secret_key']
-                        ]]
-                    ]
-                ) {
-                    withCredentials([string(credentialsId: 'vault-token', variable: 'VAULT_TOKEN')]) {
+                withVault(configuration: [vaultUrl: 'http://65.2.34.167:8200', vaultCredentialId: 'vault-token'],
+                           vaultSecrets: [[path: 'secret/aws-credentials', secretValues: [
+                               [envVar: 'AWS_ACCESS_KEY_ID', vaultKey: 'access_key'],
+                               [envVar: 'AWS_SECRET_ACCESS_KEY', vaultKey: 'secret_key']]]]) {
 
-                        echo 'Initializing S3 Backend...'
-                        sh 'terraform init'
-
-                        echo 'Validating Configuration...'
-                        sh 'terraform validate'
-
-                        echo 'Generating Execution Plan...'
-                        sh 'terraform plan -out=tfplan'
-                    }
+                    sh 'terraform init'
+                    sh 'terraform fmt -check -recursive'
+                    sh 'terraform validate'
+                    sh 'terraform plan -out=tfplan'
                 }
             }
         }
